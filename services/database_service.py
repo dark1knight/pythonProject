dc44429933a52fa65649caf69fe665d7c718f9cd
dark1knight <nakamuta26@gmail.com>
2024-11-08 14:47:33 -0400

    introducing better security measures to leave the mongodb URL safer

--- services/database_service.py
+++ services/database_service.py
@@ -1,9 +1,16 @@
 
+import os
 from pymongo import MongoClient
 
 class MongoDBContext:
     def __init__(self):
-        self.client = MongoClient("mongodb+srv://nakamutaADM:Swordman11@mycluster.hk6ns.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster")
+        # Read the MongoDB URI from the environment variable
+        mongo_uri = os.getenv("MONGO_URI")
+        if not mongo_uri:
+            raise ValueError("MONGO_URI environment variable is not set")
+        
+        # Initialize MongoDB client with the URI from the environment variable
+        self.client = MongoClient(mongo_uri)
         self._database = self.client["Call"]  
 
     def get_collection(self, collection_name):
