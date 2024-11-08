c3793db268ba9bca5420267268d145abf4f65f00
dark1knight <nakamuta26@gmail.com>
2024-11-08 14:38:19 -0400

    modified config

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 
-MONGODB_URI = "mongodb://localhost:27017"
-DATABASE_NAME = "call_records"
+import os
+
+# Default MongoDB URI with a fallback to localhost if the environment variable is not set
+MONGODB_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "call_records")
